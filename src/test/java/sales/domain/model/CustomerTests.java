b774c7c7012003d25b2a4fab9803613c7b442ee8
Carlo Luis <carlodeguzman5@gmail.com>
2015-07-07 15:43:18 +0800

    Enabled console debugging Added CustomerTests

---
+++ src/test/java/sales/domain/model/CustomerTests.java
@@ -0,0 +1,30 @@
+package sales.domain.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class CustomerTests {
+
+	@Test
+	public void testClassificationCreation() {
+		CustomerClassification telCom = new CustomerClassification(
+				"Telecommunications");
+		
+		Customer customer = new Customer("Globe", "Adrian Adame", telCom);
+		
+		assertEquals(telCom, customer.getClassification());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNameBlank() {
+		CustomerClassification telCom = new CustomerClassification(
+				"Telecommunications");
+		
+		Customer customer = new Customer("", "Adrian Adame", telCom);
+		
+	}
+	
+	
+
+}
