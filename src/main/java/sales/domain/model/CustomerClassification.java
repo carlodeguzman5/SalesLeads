c96b71413ff1ea5dc968fd4efec0d53a435d65ab
Carlo Luis De Guzman <carlodeguzman5@gmail.com>
2015-07-08 13:17:50 +0800

    Edited Model

--- src/main/java/sales/domain/model/CustomerClassification.java
+++ src/main/java/sales/domain/model/CustomerClassification.java
@@ -1,7 +1,12 @@
 package sales.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CustomerClassification {
 
+	@Id
 	private String name;
 		
 	public CustomerClassification(String name){
